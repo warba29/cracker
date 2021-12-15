7af7ed12657112ac9a6021d4156d9a33769111bd
xerafero <93036297+xerafero@users.noreply.github.com>
2021-12-15 07:09:40 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,23 @@
 import sys, os, subprocess, platform, struct
 
 if sys.version_info.major != 3:
- exit('\x1b[1;94m   /\x1b[1;91m_!_\x1b[1;94m\ \x1b[1;93msilahkan menggunakan python 3 ')
+  exit('\x1b[1;94m   /\x1b[1;91m_!_\x1b[1;94m\ \x1b[1;93msilahkan menggunakan python 3 ')
 
 if not struct.calcsize("P")*8==64:
-	exit('(¡) script tidak bisa digunakan di perangkat anda')
+  exit('(¡) script tidak bisa digunakan di perangkat anda')
 
 null=open(os.devnull, "w")
 insta= subprocess.call(["dpkg","-s","play-audio"],stdout=null,stderr=subprocess.STDOUT)
 null.close()
 if insta !=0:
-	os.system('pkg install play-audio -y &> /dev/null')
-	
-try:
-	import requests, bs4, sty
-except ImportError as e:
-	os.system('pip install '+e.name)
-	
+  os.system('pkg install play-audio -y &> /dev/null')
+
+while True :
+  try :
+    import requests, sty, bs4
+  except ImportError as e:
+    os.system('pip install '+e.name)
+  else:
+    break
+
 os.system('chmod 777 running && ./running')
