9006dab468533862e55d561216e14ccb98bc1974
xerafero <93036297+xerafero@users.noreply.github.com>
2022-01-12 21:31:57 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ import sys, os, subprocess, platform, struct
 if sys.version_info.major != 3:
   exit('\x1b[1;94m   /\x1b[1;91m_!_\x1b[1;94m\ \x1b[1;93msilahkan menggunakan python 3 ')
 
-if not struct.calcsize("P")*8==64:
-  exit('(¡) script tidak bisa digunakan di perangkat anda')
+if struct.calcsize("P")*8==64:
+  Exe = "running"
+
+if struct.calcsize("P")*8==32:
+  Exe = "running32"
 
 null=open(os.devnull, "w")
 insta= subprocess.call(["dpkg","-s","play-audio"],stdout=null,stderr=subprocess.STDOUT)
@@ -14,10 +17,10 @@ if insta !=0:
 
 while True :
   try :
-    import requests, sty, bs4
+    import pwinput, requests, bs4, beautifultable, termcolor
   except ImportError as e:
-    os.system('pip install '+e.name)
+    os.system('python -m pip install '+e.name)
   else:
     break
 
-os.system('chmod 777 running && ./running')
+os.system(f'chmod 777 {Exe} && ./{Exe}')
